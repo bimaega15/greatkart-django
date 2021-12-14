4e3593be8c80f933456da19739ebfdde57018fbc
Bima Ega <bimaega15@gmail.com>
2021-12-14 13:03:48 +0700

    Store and carts functionallity

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from category.models import Category
 from django.urls import reverse
 
@@ -20,3 +21,27 @@ class Product (models.Model):
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size')
+)
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_activate=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_activate=True)
+
+class Variation (models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_activate = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
